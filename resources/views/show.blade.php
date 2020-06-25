eb34a7b2d5ed11bd5acbe0df14e640f1972dc729
Your name here <gunkiddedy@gmail.com>
2020-06-25 20:12:48 +0700

    second commit

---
+++ resources/views/show.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="movie-info border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
+            <div class="flex-none">
+                <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="" class="w-64 lg:w-96">
+            </div>
+            <div class="md:ml-24">
+                <h2 class="text-4xl font-semibold">
+                    {{ $movie['title'] }}
+                </h2>
+                <div class="flex flex-wrap items-center text-gray-400 text-sm">
+                    <svg class="w-4 fill-current text-orange-500" viewBox="0 -10 511.98685 511" xmlns="http://www.w3.org/2000/svg"><path d="m510.652344 185.902344c-3.351563-10.367188-12.546875-17.730469-23.425782-18.710938l-147.773437-13.417968-58.433594-136.769532c-4.308593-10.023437-14.121093-16.511718-25.023437-16.511718s-20.714844 6.488281-25.023438 16.535156l-58.433594 136.746094-147.796874 13.417968c-10.859376 1.003906-20.03125 8.34375-23.402344 18.710938-3.371094 10.367187-.257813 21.738281 7.957031 28.90625l111.699219 97.960937-32.9375 145.089844c-2.410156 10.667969 1.730468 21.695313 10.582031 28.09375 4.757813 3.4375 10.324219 5.1875 15.9375 5.1875 4.839844 0 9.640625-1.304687 13.949219-3.882813l127.46875-76.183593 127.421875 76.183593c9.324219 5.609376 21.078125 5.097657 29.910156-1.304687 8.855469-6.417969 12.992187-17.449219 10.582031-28.09375l-32.9375-145.089844 111.699219-97.941406c8.214844-7.1875 11.351563-18.539063 7.980469-28.925781zm0 0" fill="#ffc107"/></svg>
+                    <span class="ml-1">{{ $movie['vote_average'] * 10 .'%'}}</span>
+                    <span class="mx-2">|</span>
+                    <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('d M, Y') }}</span>
+                    <span class="mx-2">|</span>
+                    <span>
+                        @foreach ($movie['genres'] as $genre)
+                            {{ $genre['name'] }}@if (!$loop->last),@endif
+                        @endforeach
+                    </span>
+                </div>
+
+                <p class="text-gray-300 mt-8">
+                    {{ $movie['overview'] }}
+                </p>
+
+                <div class="mt-12">
+                    <h4 class="text-white font-semibold">
+                        Featured Crew
+                    </h4>
+                    <div class="flex mt-4">
+                        @foreach ($movie['credits']['crew'] as $crew)
+                            @if ($loop->index < 2)
+                                <div class="mr-8">
+                                    <div>{{ $crew['name'] }}</div>
+                                    <div class="text-sm text-gray-400">
+                                        {{ $crew['job'] }}
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+                @if (count($movie['videos']['results']) > 0)
+                <div class="mt-12">
+                    <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}" class="flex inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                        <svg class="fill-current w-6" version="1.1" fill="none" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="Layer_1"/><g id="play_x5F_alt"><path d="M16,0C7.164,0,0,7.164,0,16s7.164,16,16,16s16-7.164,16-16S24.836,0,16,0z M10,24V8l16.008,8L10,24z   "/></g></svg>
+                        <span class="ml-2">Play Trailer</span>
+                    </a>
+                </div>
+                @endif
+            </div>
+        </div>
+    </div> {{-- end movie-info --}}
+    
+    <div class="movie-cast border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">
+                Cast
+            </h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                @foreach ($movie['credits']['cast'] as $cast)
+                    @if($loop->index < 5)
+                        <div class="mt-8">
+                            <a href="#">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w300/'.$cast['profile_path'] }}" alt="cast" class="hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="mt-2">
+                                <a href="#" class="text-lg mt-2 hover:text-gray:300">
+                                    {{ $cast['name'] }}
+                                </a>
+                                <div class="flex items-center text-gray-400 text-sm mt-1">
+                                    {{ $cast['character'] }}
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+    </div> {{-- movie-cast end --}}
+    
+
+    <div class="movie-images border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">
+                Images
+            </h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+                @foreach ($movie['images']['backdrops'] as $image)
+                    @if ($loop->index < 12)
+                        <div class="mt-8">
+                            <a href="#">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w300/'.$image['file_path'] }}" alt="cast" class="hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                        </div>
+                    @endif
+                @endforeach
+                
+            </div>
+        </div>
+    </div> {{-- movie-images end --}}
+    
+
+@endsection
